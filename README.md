abcbee70efb3cc72561c880dc6af1acf1044ebd7
ishitagarg28 <gargishita2811@gmail.com>
2025-11-02 13:04:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Headlines Hub
+
+**Headlines Hub** is a news aggregation application built using **React.js**, designed to deliver the latest top headlines and breaking news from various sources. With features like category filtering and real-time updates, it provides users with a seamless experience to stay informed.
+
+## Demo
+The API provider did not allow for free deployment. So here is the demo video link: [Demo Video](https://drive.google.com/file/d/19NZa726rbiizM1Yl9K_dU7kNQUfoy3jm/view?usp=drive_link).
+
+**Deployed link**: [Headlines Hub]() (Note: You will not be able to see any articles due to restrictions provided by the API provider).
+
+## Features
+- **Real-time News Updates**: Fetches the latest headlines from various news sources.
+- **Category Filtering**: Allows users to filter news by categories such as Sports, Technology, Health, etc.
+- **Responsive Design**: Fully responsive layout for optimal viewing on different devices.
+- **User-friendly Interface**: Clean and intuitive UI for a smooth user experience.
+
+## Technologies Used
+- **React.js**: Frontend framework for building user interfaces.
+- **Fetch API**: For making HTTP requests to fetch news articles from APIs.
+- **CSS**: For styling the application and ensuring a responsive design.
+- **React Router**: For handling navigation within the application.
+  
+## Project Structure
+ ├── public/ # Public assets ├── src/ │ ├── components/ # React components │ ├── pages/ # Different pages of the application │ ├── App.js # Main application file │ ├── index.js # Entry point of the application │ └── styles.css # CSS styles └── package.json # Project metadata and dependencies
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone 
+   cd HeadlinesHub
+   ```
+2. Install dependencies:
+   ```bash
+   npm install
+   ```
+3. Set up environment variables:
+
+   - Obtain your API key from News API and create a .env file in the root directory with the following content:
+   ```bash
+      REACT_APP_NEWS_API_KEY=your_api_key_here
+    ```
+## Run the Project
+   ```bash
+   npm start
+```
+## Usage
+- Access the application at: [Headlines Hub]()
+- Use the category filter to view news articles based on your interests.
+
+## Future Improvements
+- Search Functionality: Allow users to search for specific news articles or topics.
+- Dark Mode Toggle: Implement a dark mode option to switch between light and dark themes.
+- User Accounts: Create user accounts for personalized news feeds and bookmarks.
+- Speech-to-Text News Reader: Add an option to listen to the news articles using text-to-speech functionality.
+- Offline Mode for Cached News: Implement an offline mode where users can still read recently viewed articles even if there is no internet connection.(using Service Workers to cache articles locally )
+
+## Contributing
+- Contributions are welcome! Please fork the repository and submit a pull request.
+
+   
---
+++ src/App.js
@@ -0,0 +1,139 @@
+
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import NewsComponent from "./Components/NewsComponent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+export default class App extends Component {
+  pageSize = 8;
+  state={                                          
+    progress:0
+  }
+  setProgress=(progress)=>{
+    this.setState({progress:progress})    
+  }
+  render() {
+    return (
+      <div>
+        {" "}
+        {/* Use a valid HTML element as the root element */}
+        <Router>
+          <Navbar />
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+            
+          />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="Home"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="general"
+                  pageTitle="Home-HeadlinesHub"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/business"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="business"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="business"
+                  pageTitle="Business-HeadlinesHub"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="entertainment"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="entertainment"
+                  pageTitle="Entertainment-HeadlinesHub"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/general"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="general"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="general"
+                  pageTitle="General-HeadlinesHub"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/health"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="health"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="health"
+                  pageTitle="Health-HeadlinesHub"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="science"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="science"
+                  pageTitle="Science-HeadlinesHub"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/sports"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="sports"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="sports"
+                  pageTitle="Sports-HeadlinesHub"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/technology"
+              element={
+                <NewsComponent setProgress={this.setProgress}
+                  key="technology"
+                  pageSize={this.pageSize}
+                  country="us"
+                  category="technology"
+                  pageTitle="Technology-HeadlinesHub"
+                />
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
+}
